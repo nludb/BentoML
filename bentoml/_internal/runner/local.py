d714ddc454f7a39b5e97a5d9dd39927c96b0b1ef
Edward Benson <edward.benson@gmail.com>
2022-01-25 11:55:01 -0500

    Fix an indexing and pickling bug when a runner returns a generator instead of list

--- bentoml/_internal/runner/local.py
+++ bentoml/_internal/runner/local.py
@@ -1,5 +1,5 @@
+from types import GeneratorType
 import typing as t
-
 from .runner import Runner
 from .runner import RunnerImpl
 from .runner import RunnerState
@@ -40,6 +40,11 @@ class LocalRunner(RunnerImpl):
                 batch_result,
                 batch_axis=self._runner.batch_options.output_batch_axis,
             )
+            if isinstance(single_results, GeneratorType):
+                # A runner may mistakenly return a generator, causing 
+                # indexing & pickling errors downstream.
+                single_results = list(single_results)
+
             return single_results[0]
 
         if isinstance(self._runner, SimpleRunner):
